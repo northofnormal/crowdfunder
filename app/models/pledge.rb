8818ca52f81d261f8ffb7603e05d87da479f5934
Anne <northofnormal@gmail.com>
2013-10-03 14:58:26 -0400

    make tests for pledge model pass

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,4 +2,8 @@ class Pledge < ActiveRecord::Base
   attr_accessible :amount, :project_id, :user_id
   belongs_to :user
   belongs_to :project 
+
+  validates :amount, :numericality => { :greater_than => 0 }
+  validates :user_id, presence: true
+  validates :project_id, presence: true
 end
--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe Pledge do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "should require a user" do
+		pledge = FactoryGirl.build(:pledge, user: nil)
+
+		# should not be able to save
+		expect(pledge).to have(1).errors_on(:user_id)
+	end
+
+	it "should require a project" do
+		pledge = FactoryGirl.build(:pledge, project: nil)
+
+		# should not be able to save
+		pledge.should_not == pledge.save
+
+		expect(pledge).to have(1).errors_on(:project_id)
+	end
+
+	it "should require an amount" do
+		pledge = FactoryGirl.build(:pledge, amount: nil)
+
+		# should not be able to save
+		pledge.should_not == pledge.save
+
+		expect(pledge).to have(1).errors_on(:amount)
+	end
+
+	it "should require an amount greater than 0" do
+		pledge = FactoryGirl.build(:pledge, amount: -10)
+
+		#should not be able to save
+		pledge.should_not == pledge.save
+
+		expect(pledge).to have(1).errors_on(:amount)
+	end
+
+	it "should require the amount to be an integer" do
+		pledge = FactoryGirl.build(:pledge, amount: 'poop')
+
+		# should not be able to save
+		pledge.should_not == pledge.save
+
+		expect(pledge).to have(1).errors_on(:amount)
+	end
 end
